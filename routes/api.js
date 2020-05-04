29ffc714980d6a9ffdfbb2e9809d54a0fdd055e2
Alan Quinn <alancquinn2@gmail.com>
2020-05-04 09:45:53 +0100

    Initial Node, express & mongo set up

---
+++ routes/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/todos', (res, req, next) => {
+
+});
+
+router.post('/todos', (res, req, next) => {
+
+});
+
+router.delete('todos:id', (res, req, next) => {
+
+});
+
+module.exports = router;
